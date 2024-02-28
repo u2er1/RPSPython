dab9f22941e6dc6b5278f551f80b40c1efb25c2d
Ayoub <user127745@gmail.com>
2024-02-28 20:29:27 +0100

    main

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-import os
+from random import randint
+from tkinter import *
+
+
+t = ["Rock", "Paper", "Scissors"]
+
+#Computer 
+computer = t[randint(0,2)]
+
+def playerChoise():
+    player = input("Rock, Paper or Scissors? ")
+    return player
+
+def whoWins(player, computer):
+    #Codigo
