712910474781d4e5bd0ec9895918f4595eb5a35d
Ayoub <user127745@gmail.com>
2024-03-05 22:24:56 +0100

    Final Version

--- main.py
+++ main.py
@@ -9,23 +9,31 @@ def playerChoise():
     player = input("Rock, Paper or Scissors? ")
     return player
 
-def whowins(player, computer):
+def whoWins(player, computer):
+    print(computer)
     if player == computer:
         print("Tie!")
-    elif player == "rock":
-        if computer == "paper":
+    elif player.lower() == "rock":
+        if computer.lower() == "paper":
             print("You lose!", computer, "covers", player)
         else:
             print("You win!", player, "smashes", computer)
-    elif player == "paper":
-        if computer == "scissors":
+    elif player.lower() == "paper":
+        if computer.lower() == "scissors":
             print("You lose!", computer, "cut", player)
         else:
             print("You win!", player, "covers", computer)
-    elif player == "scissors":
-        if computer == "rock":
+    elif player.lower() == "scissors":
+        if computer.lower() == "rock":
             print("You lose...", computer, "smashes", player)
         else:
             print("You win!", player, "cut", computer)
     else:
         print("That's not a valid play. Check your spelling!")
+
+def main():
+    player = playerChoise()
+    whoWins(player, computer)
+
+
+main()
