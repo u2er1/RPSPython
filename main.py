332076065a3af742b3d70099f6ac5e690bc3e855
mrchatib <a2308929@institutmontilivi.cat>
2024-03-05 22:10:31 +0100

    def whowins

--- main.py
+++ main.py
@@ -11,5 +11,23 @@ def playerChoise():
     player = input("Rock, Paper or Scissors? ")
     return player
 
-def whoWins(player, computer):
-    #Codigo
+def whowins(player, computer):
+    if player == computer:
+        print("Tie!")
+    elif player == "rock":
+        if computer == "paper":
+            print("You lose!", computer, "covers", player)
+        else:
+            print("You win!", player, "smashes", computer)
+    elif player == "paper":
+        if computer == "scissors":
+            print("You lose!", computer, "cut", player)
+        else:
+            print("You win!", player, "covers", computer)
+    elif player == "scissors":
+        if computer == "rock":
+            print("You lose...", computer, "smashes", player)
+        else:
+            print("You win!", player, "cut", computer)
+    else:
+        print("That's not a valid play. Check your spelling!")
